1fcca0997403dca7a311dab9db487a00ebc0dd0a
하은지 <eunbyul0805@gmail.com>
2022-08-26 00:34:33 +0900

    connected page

---
+++ GreenAI/index.js
@@ -0,0 +1,11 @@
+function temp_data() {
+	return [21.9,25.1,24.6,22.9,25.1,24.2,18.7]
+}
+
+function humidity_data() {
+	return [44.0,38.9,41.6,45.7,36.0,39.8,51.6]
+}
+
+function metter_data() {
+	return [1.4,1.4,1.7,1.4,1.6,1.5,1.1]
+}
